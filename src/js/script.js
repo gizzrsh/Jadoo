17c376609abda22bdeab089454167ead897f2149
Gizzyaka <gizzatullint@bk.ru>
2026-01-25 18:54:21 +0500

    feat: Add hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log('ddata test');
+const burgerBtn = document.getElementById('burgerBtn');
+const headerMenu = document.querySelector('.header__menu');
+
+burgerBtn.addEventListener('click', function() {
+    headerMenu.classList.toggle('header__menu--active');
+    burgerBtn.classList.toggle('header__burger--active');
+    document.body.classList.toggle('hidden');
+})
