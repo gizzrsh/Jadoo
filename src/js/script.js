5d264370665708be07afb8de59a1b33295861d78
Gizzyaka <gizzatullint@bk.ru>
2026-02-01 17:42:11 +0500

    feat: Adding section Services and mobile adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
-const burgerBtn = document.getElementById('burgerBtn');
-const headerMenu = document.querySelector('.header__menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerBtn = document.getElementById('burgerBtn');
+    const headerMenu = document.querySelector('.header__menu');
 
-burgerBtn.addEventListener('click', function() {
-    headerMenu.classList.toggle('header__menu--active');
-    burgerBtn.classList.toggle('header__burger--active');
-    document.body.classList.toggle('hidden');
-})
+    burgerBtn.addEventListener('click', function() {
+        headerMenu.classList.toggle('header__menu--active');
+        burgerBtn.classList.toggle('header__burger--active');
+        document.body.classList.toggle('hidden');
+    })
+
+    // services__card--active
+    const servicesCards = document.querySelectorAll('.services__card'); 
+
+    servicesCards.forEach(card => {
+        card.addEventListener('click', () => {
+            servicesCards.forEach(e => {
+                e.classList.remove('services__card--active');
+            })
+            card.classList.add('services__card--active');
+        })
+    });
+
+});
